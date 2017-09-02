2bb719fc7034bf259d38169bfe58dcf01bb4652e
Travis CI User <travis@example.org>
2017-09-02 02:10:53 +0000

    Update documentation

---
+++ implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["goji"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"goji/struct.Transition.html\" title=\"struct goji::Transition\">Transition</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"enum\" href=\"goji/enum.Credentials.html\" title=\"enum goji::Credentials\">Credentials</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"goji/struct.Jira.html\" title=\"struct goji::Jira\">Jira</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["goji"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"https://docs.rs/serde_json/1.0.2/serde_json/error/struct.Error.html\" title=\"struct serde_json::error::Error\">SerdeError</a>&gt; for <a class=\"enum\" href=\"goji/enum.Error.html\" title=\"enum goji::Error\">Error</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"https://docs.rs/reqwest/0.7.3/reqwest/error/struct.Error.html\" title=\"struct reqwest::error::Error\">HttpError</a>&gt; for <a class=\"enum\" href=\"goji/enum.Error.html\" title=\"enum goji::Error\">Error</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/std/io/error/struct.Error.html\" title=\"struct std::io::error::Error\">IoError</a>&gt; for <a class=\"enum\" href=\"goji/enum.Error.html\" title=\"enum goji::Error\">Error</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
